9030b9cd8cf2041d302d857641391e3cb1fab08f
GertSallaerts <1267900+GertSallaerts@users.noreply.github.com>
2019-06-12 16:44:05 +0200

    [aql] add package that contains all tools

---
+++ packages/aql/index.js
@@ -0,0 +1,11 @@
+const Condition = require('@ambassify/aql-condition');
+const memoryFilter = require('@ambassify/aql-memory-filter');
+const Parser = require('@ambassify/aql-parser');
+
+module.exports = {
+    Parser,
+    Condition,
+    filter: {
+        Memory: memoryFilter
+    }
+};
