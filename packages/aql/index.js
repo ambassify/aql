de9807103f61155f4068644c1396fac2ef86d9bd
GertSallaerts <1267900+GertSallaerts@users.noreply.github.com>
2019-09-24 13:21:24 +0200

    [build] add build for browser

--- packages/aql/index.js
+++ packages/aql/index.js
@@ -1,10 +1,10 @@
-const Condition = require('@ambassify/aql-condition');
-const memoryFilter = require('@ambassify/aql-memory-filter');
-const Parser = require('@ambassify/aql-parser');
+var Condition = require('@ambassify/aql-condition');
+var memoryFilter = require('@ambassify/aql-memory-filter');
+var Parser = require('@ambassify/aql-parser');
 
 module.exports = {
-    Parser,
-    Condition,
+    Parser: Parser,
+    Condition: Condition,
     Filter: {
         memory: memoryFilter
     }
